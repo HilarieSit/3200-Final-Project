ef6ff0c162f074af452d060af96de144b6333413
Hilarie Sit <HilSit@dhcp-rhodes-3087.eduroam.cornell.edu>
2018-05-04 16:53:55 -0400

    edited

--- leastsquaresfit.m
+++ leastsquaresfit.m
@@ -3,9 +3,18 @@ DF = load('dampingforce.mat');
 
 Fsp = SF.Fsp(1,:);
 x_dot = SF.Fsp(2,:);
-sp_n = 3;
+sp_n = length(x_dot);
+sp_m = 3;
+
+[f,sp_a,sp_X,sp_G] = polylsq(x_dot,Fsp,sp_n,sp_m);
+
+disp(f)
 
 Fd = DF.Fd(1,:);
 x = DF.Fd(2,:);
+d_n = length(x);
+d_m = 2;
 
-[a,X,G] = polylsq(x_dot,Fsq,sp_n,m);
+[f,d_a,d_X,d_G] = polylsq(x,Fd,d_n,d_m);
+
+disp(f)
