874dbb1f1acb62bf9d390bfb553150ef14cb67a3
Hilarie Sit <HilSit@dhcp-hol-2035.eduroam.cornell.edu>
2018-05-05 11:18:00 -0400

    added rungekutta files

--- leastsquaresfit.m
+++ leastsquaresfit.m
@@ -6,15 +6,15 @@ x_dot = SF.Fsp(2,:);
 sp_n = length(x_dot);
 sp_m = 3;
 
-[f,sp_a,sp_X,sp_G] = polylsq(x_dot,Fsp,sp_n,sp_m);
-
-disp(f)
+[fsp,asp] = polylsq(x_dot,Fsp,sp_n,sp_m);
+disp(fsp)
+disp(asp)
 
 Fd = DF.Fd(1,:);
 x = DF.Fd(2,:);
 d_n = length(x);
 d_m = 2;
 
-[f,d_a,d_X,d_G] = polylsq(x,Fd,d_n,d_m);
-
-disp(f)
+[fd,ad] = polylsq(x,Fd,d_n,d_m);
+disp(fd)
+disp(ad)
