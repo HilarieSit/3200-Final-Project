ca0ff4f26cf2f811043b634ddf9806d94fa2239f
Jonathan Pham <jphamine@gmail.com>
2018-05-09 00:35:43 -0400

    edited least squares fit

--- leastsquaresfit.m
+++ leastsquaresfit.m
@@ -1,23 +1,58 @@
+clear all
+clc
 SF = load('springforce.mat');
 DF = load('dampingforce.mat');
 
-Fsp = SF.Fsp(1,:);
-x_dot = SF.Fsp(2,:);
-sp_n = length(x_dot);
-sp_m = 3;
+%-------------------------------------------------------
 
-[fsp,asp] = polylsq(x_dot,Fsp,sp_n,sp_m);
-disp(fsp)
+% spring force data
+Fsp = SF.Fsp(1, :); % spring force data
+x = SF.Fsp(2, :);   % displacement data
+sp_n = length(x);   % number of displacement data points
+sp_m = 3;           % sp_m is the number of unknown coefficients
+
+% compute least squares fit on spring force
+asp = polylsq(x, Fsp, sp_n, sp_m);
 disp(asp)
 
-Fd = DF.Fd(1,:);
-x = DF.Fd(2,:);
-d_n = length(x);
-d_m = 2;
+% plot spring force data and its least squares fit
+figure
+scatter(x, Fsp)
+hold on
+maxx = max(x);
+minx = min(x);
+x_vec = (minx:(maxx-minx)/50:maxx);
+Fsp_func = @(w) asp(1,1)*w + asp(2,1)*w.^2 + asp(3,1)*w.^3;
+plot(x_vec, Fsp_func(x_vec))
+title('Plot of spring force data and its least squares fit')
+xlabel('x')
+ylabel('F_{sp}')
+hold off
 
-[fd,ad] = polylsq(x,Fd,d_n,d_m);
-disp(fd)
+%-------------------------------------------------------
+
+% damping force data
+Fd = DF.Fd(1, :);       % damping force data
+x_dot = DF.Fd(2, :);    % velocity data
+d_n = length(x_dot);    % number of velocity data points
+d_m = 2;                % d_m is the number of unknown coefficients
+
+% compute least squares fit on damping force
+ad = polylsq(x_dot, Fd, d_n, d_m);
 disp(ad)
 
-% make system of ODE equations
+% plot damping force data and its least squares fit
+figure
+scatter(x_dot, Fd)
+hold on
+maxxdot = max(x_dot);
+minxdot = min(x_dot);
+xdot_vec = (minxdot:(maxxdot-minxdot)/50:maxxdot);
+Fd_func = @(w) ad(1,1)*w + ad(2,1)*w.^2;
+plot(xdot_vec, Fd_func(xdot_vec))
+title('Plot of damping force data and its least squares fit')
+xlabel('x_dot', 'Interpreter', 'none')
+ylabel('F_{d}')
+hold off
 
+% make system of ODE equations
--- polylsq.m
+++ polylsq.m
@@ -1,10 +1,10 @@
 % Performs a polynomial least squares fit given 
-function [f,a] = polylsq(x,y,n,m)
+function a = polylsq(x,y,n,m)
     % construct X matrix
-    X = zeros(n,m+1);
-    for c = 1:m+1
+    X = zeros(n,m);
+    for c = 1:m
         for r = 1:n
-            X(r,c)=x(r).^(c-1);
+            X(r,c)=x(r).^(c);
         end
     end
     
@@ -19,27 +19,4 @@ function [f,a] = polylsq(x,y,n,m)
     
     % solve the a vector
     a = G\B;
-
-    % construct scatter plot
-    figure
-    scatter(x,y)
-    hold on
-    % overlay least squares fit
-    maxx = max(x);
-    minx = min(x);
-    step = (maxx-minx)/50;
-    w = (minx:step:maxx);
-    if m == 2
-        v = a(1,1)+a(2,1)*w+a(3,1)*w.^2;
-        f = @(w) a(1,1)+a(2,1)*w+a(3,1)*w.^2;
-    end 
-    if m == 3
-        v = a(1,1)+a(2,1)*w+a(3,1)*w.^2+a(4,1)*w.^3;
-        f = @(w) a(1,1)+a(2,1)*w+a(3,1)*w.^2+a(4,1)*w.^3;
-    end 
-    plot(w,v)
-    title('Scatter plot of data and least squares fit')
-    xlabel('x')
-    ylabel('y')
-    hold off
 end
