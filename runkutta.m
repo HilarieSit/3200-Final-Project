de2546cd11405cd091342ea18826c379dcea19a0
Jonathan Pham <jphamine@gmail.com>
2018-05-11 16:24:34 -0400

    update runge kutta

--- runkutta.m
+++ runkutta.m
@@ -1,12 +1,16 @@
 function runkutta
+clear all
+clc
+
 L = 5.2;
 V = 10;
 T = L/(V*1000/3600);
 h = T/50;
 w = pi/T;
 
+% [t, y] = rk4sys(@dydtsys,[0 10],[0 0],2);
 [t y] = rk4sys(@dydtsys,[0 4],[0 0 0 0],h);
 
 plot(y,t)
 
-end 
+end
