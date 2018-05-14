bf86bc8b8fd310e5768786648ba89a3bd2cf6bb7
Hilarie Sit <hs764@cornell.edu>
2018-05-14 02:44:29 -0400

    add bisection.m

---
+++ bisection.m
@@ -0,0 +1,34 @@
+function xs = bisection(xl,xu,t,x)
+threshold = 1e-3; 
+error = 1;
+xr = 0;             %x calculated with bisection
+oldxr = 0;          %store old xr
+fl = 0;             %function evaluated at xl
+fr = 0;             %function evaluated at xu
+k = 0;              %number of iterations
+
+while error > threshold
+    fl = lagrangeIP5(xl,t,x);             %evaluate function at lower bound
+    oldxr = xr;                         %store old xr 
+    xr = (xl+xu)/2;                     %calculate midpoint
+    fr = lagrangeIP5(xr,t,x);               %evaluate function at xr
+    
+    k = k+1;
+    fprintf('For iteration %d\n',k);
+    fprintf('xl: %d\n',xl);
+    fprintf('xu: %d\n',xu);
+    fprintf('xr: %d\n',xr);
+    if fl*fr < 0                        %if root not between xl and xr
+        xu = xr;                        %set xu as xr
+    else
+        xl = xr;                        %otherwise, set xl as xr
+    end 
+   
+    if k > 1
+        error = abs(xr-oldxr)/xr;       %calculate approximate relative error
+        fprintf('error: %d\n',error);
+    end
+    fprintf('f(xr): %d\n\n',fr);
+end 
+xs = xr;
+end
