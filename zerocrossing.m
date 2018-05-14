69f190a8aab3720f476cc35447696e2683894768
Hilarie Sit <hs764@cornell.edu>
2018-05-14 02:45:37 -0400

    update zerocrossing

--- zerocrossing.m
+++ zerocrossing.m
@@ -9,6 +9,12 @@ V10_times = [t10_rk4(ind_num-2) t10_rk4(ind_num-1) t10_rk4(ind_num)...
 V10_values = [x10_rk4(ind_num-2,1) x10_rk4(ind_num-1,1) x10_rk4(ind_num,1)...
     x10_rk4(ind_num+1,1) x10_rk4(ind_num+2,1) x10_rk4(ind_num+3,1)];
 
+% bisection
+xl = x10_rk4(ind_num,1);
+xu = x10_rk4(ind_num+1,1);
+v40_xs = bisection(xl,xu,V10_times,V10_values);
+disp(v40_xs)
+
 % for V = 40
 V40B = t40_rk4 < 0.5520;    % observed zero crossing
 ind_num = length(find(V40B));
@@ -16,3 +22,9 @@ V40_times = [t40_rk4(ind_num-2) t40_rk4(ind_num-1) t40_rk4(ind_num)...
     t40_rk4(ind_num+1) t40_rk4(ind_num+2) t40_rk4(ind_num+3)];
 V40_values = [x40_rk4(ind_num-2,1) x40_rk4(ind_num-1,1) x40_rk4(ind_num,1)...
     x40_rk4(ind_num+1,1) x40_rk4(ind_num+2,1) x40_rk4(ind_num+3,1)];
+
+% bisection
+xl = x40_rk4(ind_num,1);
+xu = x40_rk4(ind_num+1,1);
+v10_xs = bisection(xl,xu,V40_times,V40_values);
+disp(v10_xs)
